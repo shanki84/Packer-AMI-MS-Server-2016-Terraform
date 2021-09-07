aba4ae211ec9d9e20a7b9a267a4153f6773d577e
shanki84 <31502997+shanki84@users.noreply.github.com>
2021-09-07 01:20:19 +0100

    Update user_data.ps1

--- Terraform/scripts/user_data.ps1
+++ Terraform/scripts/user_data.ps1
@@ -1,4 +1,26 @@
 <powershell>
+# The region associated with your bucket e.g. eu-west-1, us-east-1 etc. (see http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-regions-availability-zones.html#concepts-regions)
+$accessKey = $accesskey
+# Your account secret access key
+$secretKey = $secretkey
+$region = "eu-west-1"
+# The name of your S3 Bucket
+$bucket = "my-test-bucket"
+# The folder in your bucket to copy, including trailing slash. Leave blank to copy the entire bucket
+$keyPrefix = "my-folder/"
+# The local file path where files should be copied
+$localPath = "C:\s3-downloads\"    
+
+# Download .NET application from S3
+$objects = Get-S3Object -BucketName $bucket -KeyPrefix $keyPrefix -AccessKey $accessKey -SecretKey $secretKey -Region $region
+
+foreach($object in $objects) {
+    $localFileName = $object.Key -replace $keyPrefix, ''
+    if ($localFileName -ne '') {
+        $localFilePath = Join-Path $localPath $localFileName
+        Copy-S3Object -BucketName $bucket -Key $object.Key -LocalFile $localFilePath -AccessKey $accessKey -SecretKey $secretKey -Region $region
+    }
+}
 Function Enable-RDP
 {
 <# 
