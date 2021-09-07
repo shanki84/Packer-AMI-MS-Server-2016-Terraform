f319fdb25fd34348dade6bd7ffc85ab0cab5b40a
shanki84 <31502997+shanki84@users.noreply.github.com>
2021-09-07 01:39:04 +0100

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -90,6 +90,12 @@ resource "aws_instance" "this" {
     command = "aws s3 cp ${path.module}/secret s3://PATHTOKEYPAIR/ --recursive"
   }
 
+  #--- Copy .NET applications from S3 keys to destication dir
+  provisioner "local-exec" {
+      command = "aws s3 cp s3://myBucket/dir destination_dir --recursive"
+  }
+
+
   #--- Deletes keys on destroy
   provisioner "local-exec" {
     when    = "destroy"
